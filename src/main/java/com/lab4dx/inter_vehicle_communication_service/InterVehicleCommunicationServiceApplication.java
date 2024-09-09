e9c7dc4ab212725597b9c20d3b19494216710bb6
Tvivid <tjsaud3250@gmail.com>
2024-09-09 11:39:52 +0900

    first commit

---
+++ src/main/java/com/lab4dx/inter_vehicle_communication_service/InterVehicleCommunicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lab4dx.inter_vehicle_communication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterVehicleCommunicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterVehicleCommunicationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab4dx/inter_vehicle_communication_service/InterVehicleCommunicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab4dx.inter_vehicle_communication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterVehicleCommunicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
